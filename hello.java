e28642f41979bdb851c80b8f8bf49822ced2b8ba
bhavya <bhavyaawsacc@gmail.com>
2023-07-10 04:40:31 +0000

    master-good morning

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ class Hello
 {
 Public static void main(String args[])
 {
+<<<<<<< Updated upstream
 System.out.println(“hello-from child”);
 child
+=======
+System.out.println(“hello-from master-good morning”);
+>>>>>>> Stashed changes
 }
 }
 
