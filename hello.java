ea1dab95877cdda31d495ae97ade755c30403012
bhavya <bhavyaawsacc@gmail.com>
2023-07-10 04:42:47 +0000

    final commit

--- hello.java
+++ hello.java
@@ -2,12 +2,8 @@ class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< Updated upstream
-System.out.println(“hello-from child”);
-child
-=======
-System.out.println(“hello-from master-good morning”);
->>>>>>> Stashed changes
+System.out.println(“hello-from child-good morning”);
+0b80c2c (child-good morning)
 }
 }
 
